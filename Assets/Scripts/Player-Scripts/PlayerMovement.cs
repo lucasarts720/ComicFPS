a9a62827e760f73b8187518e29cce85d94b781a0
Gian Luca Colucci <lucasarts720@hotmail.com>
2024-09-15 21:09:33 -0300

    PlayerMovement Set-up

--- Assets/Scripts/Player-Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player-Scripts/PlayerMovement.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Remove "Escape - GetKeyDown" before release.
+
+    public Camera fpCamera;
+    public float movementSpeed = 10.0f;
+    
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float movementBackFort = Input.GetAxis("Vertical") * movementSpeed;
+        float movementSideways = Input.GetAxis("Horizontal") * movementSpeed;
+
+        movementBackFort *= Time.deltaTime;
+        movementSideways *= Time.deltaTime;
+
+        transform.Translate(movementSideways, 0, movementBackFort);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
     }
 }
